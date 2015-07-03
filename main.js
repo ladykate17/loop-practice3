3955b29d46c09fabc292a4f997913c5779735bc5
ladykate17 <stamm.katherine@gmail.com>
2015-07-03 15:19:53 -0600

    first commit

---
+++ main.js
@@ -0,0 +1,56 @@
+// JS Document
+var students = [{
+ name: 'Liz',
+ age: 25,
+ city: 'Boulder'
+},{
+ name: 'Meghan',
+ age: 27,
+ city: 'Denver'
+},{
+ name: 'Trent',
+ age: 32,
+ city: 'Boulder'
+},{
+ name: 'Chelsea',
+ age: 24,
+ city: 'Boulder'
+},{
+ name: 'Amir',
+ age: 18,
+ city: 'Denver'
+}];
+
+// --- Problem 1 -- //
+// 25
+// 27
+// 32
+// 24
+// 18
+
+for (var i = 0; i < students.length; i++){
+
+	console.log()
+	
+}
+
+
+
+
+// --- Problem 2 -- //
+// Liz, Boulder
+// Meghan, Denver
+// Trent, Boulder
+// Chelsea, Boulder
+// Amir, Denver
+ 
+// --- Problem 3 -- //
+// Liz is from Boulder
+// Trent is from Boulder
+// Chelsea is from Boulder
+ 
+
+
+// --- Problem 4 -- //
+// Meghan is older than 25
+// Trent is older than 25
