bc430cfd28b786eaac56717b5f0be335ea38508b
Katie Stamm <katiestamm@Katies-MacBook-Pro.local>
2015-07-03 18:47:39 -0600

    completed exercise

--- main.js
+++ main.js
@@ -27,10 +27,10 @@ var students = [{
 // 32
 // 24
 // 18
+// (var i = 0; i < students.length; i++)
+for (var i = 0; i < students.length; i++) {
 
-for (var i = 0; i < students.length; i++){
-
-	console.log()
+	console.log( students[i].age );
 	
 }
 
@@ -43,14 +43,33 @@ for (var i = 0; i < students.length; i++){
 // Trent, Boulder
 // Chelsea, Boulder
 // Amir, Denver
+
+for (var i = 0; i < students.length; i++) {
+
+	console.log( students[i].name + ', ' + students[i].city );
+	
+}
+
  
 // --- Problem 3 -- //
 // Liz is from Boulder
 // Trent is from Boulder
 // Chelsea is from Boulder
- 
+ for (var i = 0; i < students.length; i++) {
+
+ 	if (students[i].city === "Boulder") {
+		console.log( students[i].name + ' is from ' + students[i].city );
+	}
+}
 
 
 // --- Problem 4 -- //
 // Meghan is older than 25
 // Trent is older than 25
+ for (var i = 0; i < students.length; i++) {
+
+ 	if (students[i].age > 25) {
+		console.log( students[i].name + ' is older than 25' );
+	}
+}
+
